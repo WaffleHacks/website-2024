309df44db3a772f10c9965c2be4ec8b604e02f67
Mike Odnis <airwolf635@gmail.com>
2024-05-16 14:49:46 -0400

    chore: linting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-  return (
-    <div className='fixed z-50 bg-white w-screen flex flex-row gap-8 justify-center items-center py-6 shadow-lg font-semibold'>
-        <a href="#about">About</a>
-        <a href="#tracks">Tracks</a>
-        <a href="#calendar">Calendar</a>
-        <img src="/assets/images/header.svg" alt="" />
-        <a href="#sponsors">Sponsors</a>
-        <a href="#faqs">FAQs</a>
-        <a href="#apply">Apply Now</a>
-    </div>
-  )
-}
+	return (
+		<div className="fixed z-50 bg-white w-screen flex flex-row gap-8 justify-center items-center py-6 shadow-lg font-semibold">
+			<a href="#about">About</a>
+			<a href="#tracks">Tracks</a>
+			<a href="#calendar">Calendar</a>
+			<img src="/assets/images/header.svg" alt="" />
+			<a href="#sponsors">Sponsors</a>
+			<a href="#faqs">FAQs</a>
+			<a href="#apply">Apply Now</a>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
