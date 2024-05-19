460bf3fced6ef39815672acd7286ad62173c65c8
Ethan Horowitz <ethan.horowitz@duke.edu>
2024-05-19 15:01:11 -0400

    fixed faq and added sponsors

--- src/components/FaqPanel.tsx
+++ src/components/FaqPanel.tsx
@@ -47,7 +47,7 @@ const FaqPanel = () => {
 			<div className="block w-full" style={{ maxWidth: "min(100vw, 80rem)" }}>
 				{/* <p>For all your questions, here are some answers!</p>
             <h1 className="text-4xl md:text-5xl">FAQs</h1> */}
-				<h1 className="text-4xl font-extrabold mb-4">Sponsors</h1>
+				<h1 className="text-4xl font-extrabold mb-4">FAQs</h1>
 
 				<div className="flex flex-col md:flex-row gap-4 mt-4">
 					<div className="flex flex-col w-full md:w-1/2 gap-4">
