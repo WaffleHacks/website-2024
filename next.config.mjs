fa4e883c92d8422e0e5f7609131a600c6fa8deae
Mike Odnis <airwolf635@gmail.com>
2024-06-02 18:27:58 -0400

    chore: uncommented something important, in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -23,11 +23,11 @@ const config = {
 		ignoreBuildErrors: true,
 	},
 	reactStrictMode: true,
-	// logging: {
-	// 	fetches: {
-	// 		fullUrl: true,
-	// 	},
-	// },
+	logging: {
+		fetches: {
+			fullUrl: true,
+		},
+	},
 	images: {
 		remotePatterns: [
 			{
